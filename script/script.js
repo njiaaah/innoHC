79bcde0b54e556f2d1cba5409dd7281afc279826
Rostislav Kupriyanov <83307529+njiaaah@users.noreply.github.com>
2023-10-18 07:58:32 +0300

    3
     section location with map

---
+++ script/script.js
@@ -0,0 +1,52 @@
+
+    $(document).ready(function() {
+        
+        const maps = document.querySelectorAll('.section-location-map');
+        const mapZoomInBtns = document.querySelectorAll('.map-zoom-in-btn')
+        const mapZoomOutBtns = document.querySelectorAll('.map-zoom-out-btn')
+
+        // SPEED OF ZOOMING IN/OUT
+        let scaleSpeed = parseFloat(0.25)
+
+        for (let i = 0; i < maps.length; i++) {
+            mapZoomInBtns[i].addEventListener('click', ()=>{
+                // MAX ZOOM IN
+                if(maps[i].dataset.scale < parseFloat(2.5)) {
+                    let newScale = parseFloat(maps[i].dataset.scale) + scaleSpeed
+                    maps[i].style.transform = 'scale(' + newScale + ',' + newScale + ')'
+                    maps[i].dataset.scale = newScale
+                }
+            })      
+        }
+        for (let i = 0; i < maps.length; i++) {
+            mapZoomOutBtns[i].addEventListener('click', ()=>{
+                // MAX ZOOM OUT (DO NOT SET LESS THAN 1)
+                if(maps[i].dataset.scale > parseFloat(1)) {
+                    let newScale = parseFloat(maps[i].dataset.scale) - scaleSpeed
+                    maps[i].style.transform = 'scale(' + newScale + ',' + newScale + ')'
+                    maps[i].dataset.scale = newScale
+                }
+            })      
+        }
+
+        // TABS
+
+        const sectionLocTabsBtns = document.querySelector('.section-location-tab-wrap').children
+        const sectionLocTabs = document.querySelectorAll('.section-location-content-item')
+
+        for (let i = 0; i < sectionLocTabs.length; i++) {
+            sectionLocTabsBtns[i].addEventListener('click',()=>{
+                for (let x = 0; x < sectionLocTabs.length; x++) {
+
+                    // hide and apply bottom border to selected tab btn
+                    sectionLocTabsBtns[x].classList.remove('section-location-tab-selected')
+                    sectionLocTabsBtns[i].classList.add('section-location-tab-selected')
+
+                    // hide and show need tab
+                    sectionLocTabs[x].style.display = 'none'
+                    sectionLocTabs[i].style.display = 'grid'
+                }
+            })
+            
+        }
+    });
