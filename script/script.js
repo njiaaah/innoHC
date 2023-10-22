6862a020707961cdf87a65ccb133c7c29ff2afa9
Albotov Timur <sweciti@rambler.ru>
2023-10-22 19:29:43 +0300

    button up

--- script/script.js
+++ script/script.js
@@ -471,3 +471,22 @@ $('#togg-infban').click(function(){
       
     }
 })
+
+//button up
+
+const mybutton = document.getElementById("topbutton");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none"
+  }
+}
+
+function topscroll() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
