85c95ab75ccd2d653ec2f1a45ff56d525dcfae4f
Rostislav Kupriyanov <83307529+njiaaah@users.noreply.github.com>
2023-11-06 16:38:23 +0300

    catalogue.html mobile fixes

--- script/catalogue.js
+++ script/catalogue.js
@@ -1,14 +1,14 @@
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener('DOMContentLoaded', () => {
 
     // apparts heading on mobile z-index fix
 
     const appartHeading = document.querySelector('.section-filter-heading-wrap')
     const toggleFilter = document.querySelector('#toggleFilter')
-    
 
-    if(toggleFilter) {
-        window.addEventListener('change', ()=>{
-            if(toggleFilter.checked) {
+
+    if (toggleFilter) {
+        window.addEventListener('change', () => {
+            if (toggleFilter.checked) {
                 appartHeading.style.zIndex = '6'
             } else {
                 appartHeading.style.zIndex = '1'
@@ -17,29 +17,29 @@ document.addEventListener('DOMContentLoaded',() => {
     }
 
     // filter list on mobile
-    
-    $('.filterSortListHeadingMoblie').on('click', function(){
+
+    $('.filterSortListHeadingMoblie').on('click', function () {
         $('#filter-sort-dropdown-toggle').prop('checked', false)
-        $('.filter-sort-dropdown').animate({'opacity': '0'},1)
+        $('.filter-sort-dropdown').animate({ 'opacity': '0' }, 1)
         $('.filter-sort-dropdown').transition({ y: '500px' });
         $('.filter-sort-dropdown').hide(1)
     })
 
     const dropdownChk = document.querySelector('#filter-sort-dropdown-toggle')
-    const togglePlan = document.querySelector('#cataloguePlanSwitch')
+    // const togglePlan = document.querySelector('#cataloguePlanSwitch')
 
-    window.addEventListener('resize', ()=>{
+    window.addEventListener('resize', () => {
         if (window.innerWidth >= 561) {
             // console.log('desktop')
             $('#filter-sort-dropdown-toggle').prop('checked', false)
             $('.filter-sort-dropdown').show(1)
-            $('.filter-sort-dropdown').animate({'opacity': '1'},1)
-            $('.filter-sort-dropdown').transition({ y: '0px' },1);
+            $('.filter-sort-dropdown').animate({ 'opacity': '1' }, 1)
+            $('.filter-sort-dropdown').transition({ y: '0px' }, 1);
         } else {
             // console.log('mobile')
-            
-            $('.filter-sort-dropdown').css({'opacity': '0'},1)
-            $('.filter-sort-dropdown').transition({ y: '500px' },1);
+
+            $('.filter-sort-dropdown').css({ 'opacity': '0' }, 1)
+            $('.filter-sort-dropdown').transition({ y: '500px' }, 1);
             $('.filter-sort-dropdown').hide(1)
         }
 
@@ -49,19 +49,44 @@ document.addEventListener('DOMContentLoaded',() => {
     })
 
 
-    if(dropdownChk) {
-        dropdownChk.addEventListener('change', ()=>{
-            if(dropdownChk.checked  && window.innerWidth < 561) {
+    if (dropdownChk) {
+        dropdownChk.addEventListener('change', () => {
+            if (dropdownChk.checked && window.innerWidth < 561) {
 
                 $('.filter-sort-dropdown').show()
-                $('.filter-sort-dropdown').animate({'opacity': '1'},50)
+                $('.filter-sort-dropdown').animate({ 'opacity': '1' }, 50)
                 $('.filter-sort-dropdown').transition({ y: '0px' }, 350);
 
-            } else if(window.innerWidth < 561) {
-                $('.filter-sort-dropdown').animate({'opacity': '0'},1)
-                $('.filter-sort-dropdown').transition({ y: '500px' },1);
+            } else if (window.innerWidth < 561) {
+                $('.filter-sort-dropdown').animate({ 'opacity': '0' }, 1)
+                $('.filter-sort-dropdown').transition({ y: '500px' }, 1);
                 $('.filter-sort-dropdown').hide(1)
             }
         })
     }
+
+    // close dropdown/list
+
+    if (document.querySelector('.mobile-dropdown-list-modal')) {
+        var modalDropdownOptions = document.querySelector('.mobile-dropdown-list-modal').children
+        console.log(modalDropdownOptions.length)
+
+        for (let i = 0; i < modalDropdownOptions.length; i++) {
+            modalDropdownOptions[i].addEventListener('click', () => {
+
+                $('.filter-sort-dropdown').delay(150).animate({ 'opacity': '0' }, 150)
+                // $('.filter-sort-dropdown').delay(0).transition({ y: '500px' }, 300)
+                $('.filter-sort-dropdown').delay(150).hide(150)
+
+
+                setTimeout(() => {
+                    $('#filter-sort-dropdown-toggle').prop('checked', false)
+                }, 300)
+            })
+        }
+    }
+
+
+
+
 })
