c3ab83683d6ec966a8dd4a40f3766e3161c76ddf
Rostislav Kupriyanov <83307529+njiaaah@users.noreply.github.com>
2023-11-08 12:01:00 +0300

    mobilefilter fix

--- script/mobilefilter.js
+++ script/mobilefilter.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-
-    // document.getElementById('toggleFilter').checked = true
+    // mobilefilter.html открывается только из <560 вьюпорта
+    // откидываем в каталог если вьюпорт становится >=560
+    window.addEventListener('resize', () => {
+        if (window.innerWidth > 559) {
+            window.open('catalogue.html')
+        }
+    })
 
 })
