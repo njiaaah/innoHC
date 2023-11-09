3b0d17e77121e35af8010b5ef608c8a8fd1ff669
Rostislav Kupriyanov <83307529+njiaaah@users.noreply.github.com>
2023-11-09 10:31:31 +0300

    cock

--- script/modal.js
+++ script/modal.js
@@ -90,7 +90,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // close mobile sort modal if resized
     window.addEventListener('resize', () => {
         if (window.innerWidth > 559) {
-            console.log('cock')
             document.querySelector('#modalMobileSort').close()
         }
     })
