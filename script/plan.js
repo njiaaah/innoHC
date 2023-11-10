7283274e52034275d8592d5270c31de874a964c9
Albotov Timur <kpacota@lenta.ru>
2023-11-10 18:43:35 +0300

    720 complete

--- script/plan.js
+++ script/plan.js
@@ -27,3 +27,31 @@ if (document.querySelector('.section-apartament-tabs'))   {
         
     }
     }
+
+if (document.querySelector('.section-apartament-tabs-720')) {
+    const sectionApartamentsTabs720 = document.querySelector('.section-apartament-tabs-720').children
+    const sectionApartamentsImg720 = document.querySelectorAll('.apartament-img-720')
+    const sectionApartamentsRight720 = document.querySelector('.apartaments-right-720')
+
+    for (let i = 0; i < sectionApartamentsTabs720.length; i++) {
+        sectionApartamentsTabs720[i].addEventListener('click',()=>{
+            for (let x = 0; x < sectionApartamentsTabs720.length; x++) {
+    
+                sectionApartamentsTabs720[x].classList.remove('section-location-tab-selected')
+                sectionApartamentsTabs720[i].classList.add('section-location-tab-selected')
+    
+                sectionApartamentsImg720[x].style.display = 'none'
+                sectionApartamentsImg720[i].style.display = 'block'
+            }
+
+            if (sectionApartamentsImg720[2].style.display == 'block') {
+                sectionApartamentsRight720.style.display = "block"
+            }
+            
+            else {
+                sectionApartamentsRight720.style.display = "none"
+            }
+        })
+    }        
+
+}
